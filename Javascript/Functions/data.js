1ba5eb5eca08f7b4c38d02a33e5a558d3f149b99
vmurali100 <vmurali100@gmail.com>
2020-09-30 07:06:55 +0530

    Adding Array of Objects

---
+++ Javascript/Functions/data.js
@@ -0,0 +1,42 @@
+var data = [
+    {
+        "fname": "Althea",
+        "lname": "Davila",
+        "address": "5528 Sed St",
+        "city": "Boynton Beach",
+        "state": "WV",
+        "zip": 45463
+    },
+    {
+        "fname": "Gina",
+        "lname": "Hencmann",
+        "address": "4379 Vitae Rd",
+        "city": "Winter Garden",
+        "state": "PA",
+        "zip": 53768
+    },
+    {
+        "fname": "Jerry",
+        "lname": "Olson",
+        "address": "3058 Scelerisque St",
+        "city": "Winamac",
+        "state": "FL",
+        "zip": 82836
+    },
+    {
+        "fname": "Destinee",
+        "lname": "Ghiselli",
+        "address": "4303 Massa Rd",
+        "city": "Bridgeport",
+        "state": "LA",
+        "zip": 87882
+    },
+    {
+        "fname": "Marian",
+        "lname": "Garbe",
+        "address": "8664 Lacus Ln",
+        "city": "Emmaus",
+        "state": "NE",
+        "zip": 26927
+    }
+]
