de2e753b4e47e72fa65dab714d95c2a79e3023d1
vmurali100 <vmurali100@gmail.com>
2020-10-16 07:25:59 +0530

    Added Routing

---
+++ Angular/app1/projects/crud/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FeaturesComponent } from './features/features.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PricingComponent,
+    FeaturesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- Angular/app1/src/app/sibling1/sibling1.component.ts
+++ Angular/app1/src/app/sibling1/sibling1.component.ts
@@ -9,19 +9,22 @@ import { CommonService } from '../common.service';
   styleUrls: ['./sibling1.component.css']
 })
 export class Sibling1Component implements OnInit {
-  message="Hello world Message From Sibling1"
+  message="Hello world Message From Sibling1";
+  isEdit = false
   // Adding a Cammon Service as a Dependency Injection
   constructor(private cammonService : CommonService,private _http:HttpClient) { }
   allUsers
   // Declaring a Module Driven Form
   userForm = new FormGroup({
     fname:new FormControl(),
-    lname:new FormControl()
+    lname:new FormControl(),
+    id:new FormControl()
   })
 
   getUser(){
     this._http.post("http://localhost:3000/users",this.userForm.value).subscribe(res=>{
       this.getAllUsers()
+      this.userForm.reset()
     })
   }
   ngOnInit(): void {
@@ -32,11 +35,23 @@ export class Sibling1Component implements OnInit {
     this._http.get("http://localhost:3000/users").subscribe(res=>{
      this.allUsers= res;
     })
-    
   }
   deleteUser(user){
     this._http.delete("http://localhost:3000/users/"+user.id).subscribe(res=>{
       this.getAllUsers()
+    })
+  }
+
+  editUser(user){
+    this.isEdit = true
+    this.userForm.setValue(user)
+  }
+  updateUser(){
+    console.log(this.userForm.value)
+    this._http.put("http://localhost:3000/users/"+this.userForm.value.id,this.userForm.value).subscribe(res=>{
+      this.getAllUsers()
+      this.userForm.reset()
+      this.isEdit = false
     })
   }
   // sendMessageToSibling2(){
